331c6936f7aab3face03816a270e0d4fd104c343
unknown <prophetyordanov@gmail.com>
2021-02-22 07:01:49 +0200

    Added error alert to the add task page

--- src/Components/FrontEnd/Components/AddProject.js
+++ src/Components/FrontEnd/Components/AddProject.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavbarComponents from './Navbar'
-import {FormControl, Button} from 'react-bootstrap'
+import {FormControl, Button, Alert} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar'
 import '../SCSS/addProjectStyling.css'
@@ -11,12 +11,23 @@ import $ from 'jquery'
 import {uuid} from 'uuidv4'
 function AddProject() {
     const addProject=()=>{
-        store.dispatch({type: 'addProject', projectName:$('.addProjectNameForm').val(), projectDeadLine:$('.addProjectDeadlineForm').val(), id: uuid(), isFinished: false})
+        if($('.addProjectNameForm').val().length>0 && $('.addProjectDeadlineForm').val().length>0 ){
+            store.dispatch({type: 'addProject', projectName:$('.addProjectNameForm').val(), projectDeadLine:$('.addProjectDeadlineForm').val(), id: uuid(), isFinished: false})
+            window.location.href='/projects'
+        }
+        else{
+            $('.alertWrongData').fadeIn()
+        }
+    }
+    const hideAlert=()=>{
+        $('.alertWrongData').fadeOut()
+
     }
     return (
         <div className='formContainer'>
             <Navbar/>
             <div className='formHolder'>
+                <Alert className='alertWrongData alert-danger' onClick={()=>hideAlert()}><strong>Invalid project details!</strong></Alert>
                 <FormControl className='addProjectNameForm' placeholder='Enter project name'/>
                 <FormControl className='addProjectDeadlineForm' placeholder="Enter project's deadline"/>
                 <Button className='addProjectButton' onClick={()=>addProject()}><strong>Add project <BsCaretRightFill size={20 }/></strong></Button>
